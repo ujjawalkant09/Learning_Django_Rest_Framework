13a131e24c1ba3e4681859001ed7c8a8cf00e26e
ujjawalkant09 <48159084+ujjawalkant09@users.noreply.github.com>
2024-01-03 23:00:15 +0530

    ViewSets and ModelViews

--- simpleblog/posts/views.py
+++ simpleblog/posts/views.py
@@ -1,97 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django.shortcuts import render,get_object_or_404
+from rest_framework import viewsets,status
 from rest_framework.request import Request
+from rest_framework.response import Response
+from posts.serializers import PostSerializer
 from .models import Post
-from .serializers import PostSerializer
-from django.shortcuts import get_object_or_404
-# Create your views here.
-@api_view(http_method_names=['GET','POST'])
-def home(request: Request):
+# Create your views here 
+
+# class PostViewset(viewsets.ViewSet):
+    # def list(self,request:Request):
+    #     queryset = Post.objects.all()
+    #     serializer = PostSerializer(instance=queryset,many=True)
+    #     return Response(data=serializer.data,status=status.HTTP_200_OK)
     
-    if request.method == "POST":
-        data = request.data 
-        response = {"messages":"Hello World","data":data}
-        return Response(data=response,status=status.HTTP_201_CREATED)
-
-    response = {"message": "Hello World"}
-    return Response(data = response,status=status.HTTP_200_OK)
-
-
-@api_view(http_method_names=["GET","POST"])
-def list_post(request:Request):
+    # def retrieve(self,request:Request,pk=None):
+    #     post = get_object_or_404(Post,pk=pk)
+    #     serializer = PostSerializer(instance=post)
+    #     return Response(data=serializer.data,status=status.HTTP_200_OK)
     
-    if request.method == "POST":
-        data = request.data
-        serilizer = PostSerializer(data=data)
-        if serilizer.is_valid():
-            serilizer.save()
-            response = {
-                "message": "Post has Been Created",
-                "data": serilizer.data
-            }
-            return Response(data= response,status=status.HTTP_201_CREATED)
-        return Response(data=serilizer.errors,status=status.HTTP_400_BAD_REQUEST)
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
     
-    posts = Post.objects.all()
-    serilizer = PostSerializer(instance=posts, many=True)
-    response = {
-        "message":"posts",
-        "data":serilizer.data
-    }
-    return Response(data=response, status=status.HTTP_200_OK)
-
-@api_view(http_method_names=["POST"])
-def post_content(request:Request):
-    data = request.data
-    serilizer = PostSerializer(data=data)
-    if serilizer.is_valid():
-        serilizer.save()
-        response = {
-            "message": "Post has Been Created",
-            "data": serilizer.data
-        }
-        return Response(data= response,status=status.HTTP_201_CREATED)
-    return Response(data=serilizer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-
-
-@api_view(http_method_names=["GET"])
-def post_details(request:Request, post_id:int):
-    post = get_object_or_404(Post,pk=post_id)
-    serializer = PostSerializer(instance=post)
-    response = {
-        "message":"post",
-        "data":serializer.data
-    }
-
-    return Response(data=response,status=status.HTTP_200_OK)
-  
-# @api_view(http_method_names=["GET"])
-# def get_post_by_id(request:Request,post_id:int):
-#     pass
-
-@api_view(http_method_names=["PUT"])
-def update_post(request:Request,post_id:int):
-    post = get_object_or_404(Post,pk=post_id)
-    data = request.data
-    serializer = PostSerializer(instance=post,data=data)
-    if serializer.is_valid():
-        serializer.save()
-        response = {
-            "message":"Post Updated Successfully",
-            "data":serializer.data
-        }
-        return Response(data=response,status=status.HTTP_200_OK)
-    return Response(data=serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
     
 
-
-@api_view(http_method_names=["DELETE"])
-def delete_post(request:Request,post_id:int):
-    post = get_object_or_404(Post,pk=post_id)
-    post.delete()
-    return Response(status=status.HTTP_204_NO_CONTENT)
-    
--- simpleblog/simpleblog/urls.py
+++ simpleblog/simpleblog/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from posts.views import PostViewSet
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register("", PostViewSet, basename="posts")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include("posts.urls"))
+    path('posts/', include(router.urls))
 ]
