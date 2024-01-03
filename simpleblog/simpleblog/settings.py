76239299a11531f6a0eb67177208ce759ea9b6e6
ujjawalkant09 <48159084+ujjawalkant09@users.noreply.github.com>
2024-01-04 04:01:06 +0530

    custom_user_model_for_auth

--- simpleblog/simpleblog/settings.py
+++ simpleblog/simpleblog/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
 
     #Third-Party-Apps
     'rest_framework',
+    'accounts',
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
